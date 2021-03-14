9bf2c8046e7e4c6433bcf00a82ac50d0761c1614
Matheus <joaoomatheus@hotmail.com>
2021-03-14 17:13:14 -0300

    added a style to the buttons and little modifications to the App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,14 +11,10 @@ export default function App() {
       <h1>Counter: {counter}</h1>
       <button onClick={() => dispatch(increment(1))}>+1</button>
       <button onClick={() => dispatch(decrement(1))}>-1</button>
-      <br></br>
       <button onClick={() => dispatch(increment(3))}>+3</button>
       <button onClick={() => dispatch(decrement(3))}>-3</button>
-      <br></br>
       <button onClick={() => dispatch(increment(7))}>+7</button>
       <button onClick={() => dispatch(decrement(7))}>-7</button>
-
-      <br></br>
       <button onClick={() => dispatch(login())}>LOGIN</button>
       {isLogged && <p>Confidential information for users only</p>}
     </div>
